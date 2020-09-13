0ea0fe3838d5cf29da065c65afb09a19d10d2a1f
Brian Dennis <briandennis@datamachines.io>
2020-09-13 16:14:30 -0400

    Initial commit of logconfig module, application to click group

--- {{cookiecutter.hyphenated}}/{{cookiecutter.underscored}}/cli.py
+++ {{cookiecutter.hyphenated}}/{{cookiecutter.underscored}}/cli.py
@@ -1,21 +1,34 @@
 import click
 
+from .logconfig import DEFAULT_LOG_FORMAT, logging_config
+
 
 @click.group()
 @click.version_option()
-def cli():
+@click.option(
+    "--log-format",
+    type=click.STRING,
+    default=DEFAULT_LOG_FORMAT,
+    help="Python logging format string",
+)
+@click.option(
+    "--log-level", default="ERROR", help="Python logging level", show_default=True
+)
+@click.option(
+    "--log-file",
+    help="Python log output file",
+    type=click.Path(dir_okay=False, writable=True, resolve_path=True),
+    default=None,
+)
+def cli(log_format, log_level, log_file):
     "{{ cookiecutter.description }}"
+
+    logging_config(log_format, log_level, log_file)
 
 
 @cli.command(name="command")
-@click.argument(
-    "example"
-)
-@click.option(
-    "-o",
-    "--option",
-    help="An example option",
-)
-def first_command(example, option):
+@click.argument("example")
+def first_command(example):
     "Command description goes here"
+
     click.echo("Here is some output")
