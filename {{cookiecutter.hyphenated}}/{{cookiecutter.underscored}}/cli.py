f4ed385df5bf8d6b2091946463ce7e53a01dcfca
Brian Dennis <briandennis@datamachines.io>
2020-09-16 05:54:53 -0400

    Add in a trivial example of logging

--- {{cookiecutter.hyphenated}}/{{cookiecutter.underscored}}/cli.py
+++ {{cookiecutter.hyphenated}}/{{cookiecutter.underscored}}/cli.py
@@ -1,3 +1,5 @@
+import logging
+
 import click
 
 from .logconfig import DEFAULT_LOG_FORMAT, logging_config
@@ -32,3 +34,4 @@ def first_command(example):
     "Command description goes here"
 
     click.echo("Here is some output")
+    logging.info("Here's some log output)
