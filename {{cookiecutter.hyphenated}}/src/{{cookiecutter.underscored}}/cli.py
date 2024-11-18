4084997bed0586ee43d6c7e931f9522d21a93af4
Brian Dennis <bdennis+github@netrias.com>
2024-11-18 18:29:43 -0500

    Update logging and cli default behaviors

--- {{cookiecutter.hyphenated}}/src/{{cookiecutter.underscored}}/cli.py
+++ {{cookiecutter.hyphenated}}/src/{{cookiecutter.underscored}}/cli.py
@@ -3,37 +3,54 @@ from pathlib import Path
 import click
 
 from click_default_group import DefaultGroup
-from .logconfig import DEFAULT_LOG_FORMAT, logging_config
+from .logconfig import DEFAULT_LOG_FORMAT, LOGURU_LEVEL_NAMES, logging_config
 
 
-@click.group(cls=DefaultGroup, default="about", default_if_no_args=True)
+@click.group(cls=DefaultGroup, default="about", default_if_no_args=True, invoke_without_command=True)
 @click.version_option()
 @click.option(
     "--log-format",
     type=click.STRING,
+    envvar="HADRONIC_LOG_FORMAT",
+    show_envvar=True,
     default=DEFAULT_LOG_FORMAT,
-    help="Python logging format string",
+    help="Loguru logging format string",
 )
 @click.option(
-    "--log-level", default="SUCCESS", help="Python logging level", show_default=True
+    "--log-level",
+    type=click.Choice(LOGURU_LEVEL_NAMES + ["MSG"], case_sensitive=False),
+    default="SUCCESS",
+    help="Loguru logging level",
+    envvar="HADRONIC_LOG_LEVEL",
+    show_envvar=True,
+    show_default=True,
 )
 @click.option(
     "--log-file",
-    help="Python log output file",
+    help="Loguru log output file",
     type=click.Path(dir_okay=False, writable=True, resolve_path=True, path_type=Path),
+    envvar="HADRONIC_LOG_FILE",
+    show_envvar=True,
     default=None,
 )
+@click.option(
+    "--log-colorize / --no-log-colorize",
+    help="Colorize log messages by default",
+    envvar="HADRONIC_LOG_COLORIZE",
+    show_envvar=True,
+    default=True,
+)
 @click.pass_context
-def cli(ctx, log_format, log_level, log_file):
+def cli(ctx, log_format, log_level, log_file, log_colorize):
     "{{ cookiecutter.description }}"
 
     ctx.ensure_object(dict)
 
-    ctx.obj["logger"] = logging_config(log_format, log_level, log_file)
+    ctx.obj["logger"] = logging_config(log_format, log_level, log_file, log_colorize)
     ctx.obj["logger"].enable("{{ cookiecutter.underscored }}")
 
 
-@cli.command(name="about")
+@cli.command(name="about", default=True)
 @click.pass_context
 def about(ctx):
     """{{ cookiecutter.description }}
--- {{cookiecutter.hyphenated}}/src/{{cookiecutter.underscored}}/logconfig.py
+++ {{cookiecutter.hyphenated}}/src/{{cookiecutter.underscored}}/logconfig.py
@@ -1,16 +1,50 @@
+import inspect
+import logging
 import sys
+
 from functools import partialmethod
 
 from loguru import logger
 
-logger.disable("{{ cookiecutter.underscored }}")
+logger.disable("hops")
+
+DEFAULT_LOG_FORMAT = "<y>{level:<7}</y>|{elapsed}|{time:YYYY/MM/DD HH:mm:ssZ!UTC}|{name}|{function}|{message}"
+
+LOGURU_LEVEL_NAMES = [
+    "TRACE",
+    "DEBUG",
+    "INFO",
+    "SUCCESS",
+    "WARNING",
+    "ERROR",
+    "CRITICAL",
+]
 
 
-DEFAULT_LOG_FORMAT = (
-    "{level:<7}|{elapsed}|{time:YYYY/MM/DD HH:mm:ssZ!UTC}|{name}|{function}|{message}"
-)
+class InterceptHandler(logging.Handler):
+    def emit(self, record: logging.LogRecord) -> None:
+        # Get corresponding Loguru level if it exists.
+        level: str | int
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
 
-def logging_config(log_format, log_level, log_file):
+        # Find caller from where originated the logged message.
+        frame, depth = inspect.currentframe(), 0
+        while frame and (depth == 0 or frame.f_code.co_filename == logging.__file__):
+            frame = frame.f_back
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
+
+# This might be needed if a library dependency imports a
+# logging.logger at module top level
+# logging.basicConfig(handlers=[InterceptHandler()], level=0,# force=True)
+
+def logging_config(log_format, log_level, log_file,
+                   colorize=True,
+                   intercept_stdlib_logging=True):
 
     logger.remove()
     logger_msg_level = logger.level("SUCCESS").no - 1
@@ -19,16 +53,16 @@ def logging_config(log_format, log_level, log_file):
     logger.__class__.msg = partialmethod(logger.__class__.log, "MSG")
 
     log_config = {}
-    log_config["handlers"] = [
-        {"sink": sys.stderr,
-         "format": log_format, "level": log_level,
-         "colorize": True,},
-    ]
+    log_config["handlers"] = [ {"sink": sys.stderr,
+                                "format": log_format,
+                                "level": log_level,
+                                "colorize": colorize,}, ]
 
     if logger.level(log_level).no >= logger_msg_level:
         log_config["handlers"].append(
-            {"sink": sys.stdout, "format": "<y>{level}</y> - {message}",
-             "colorize": True,
+            {"sink": sys.stderr,
+             "format": "<y>{level}</y> - {message}",
+             "colorize": colorize,
              "level": "MSG",
              "filter": lambda record: record["level"].name == "MSG"
              }
@@ -40,4 +74,7 @@ def logging_config(log_format, log_level, log_file):
             )
 
     logger.configure(**log_config)
+    if intercept_stdlib_logging:
+        logging.basicConfig(handlers=[InterceptHandler()], level=0, force=True)
     return logger
+
